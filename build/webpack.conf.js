195c4b3fa56d0c2978469397a1cd22845300446d
zhuyimin <zhuyimin@meizu.com>
2017-08-30 14:35:00 +0800

    add ExtractTextPlugin

--- build/webpack.conf.js
+++ build/webpack.conf.js
@@ -2,31 +2,35 @@ var path = require('path')
 var outputRoot = path.join(__dirname, '../output');
 var srcRoot = path.join(__dirname, '../src');
 
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
 module.exports = {
-  entry: {
-    bundle: path.join(srcRoot,'./main.js')
-  },
-  output: {
-    path: outputRoot,
-    filename: '[name].js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        use: [
-            {loader: 'babel-loader'}
-        ],
-        include: srcRoot
-      },
-      {
-          test: /\.css$/,
-          use: [
-              {loader: 'style-loader'},
-              {loader: 'css-loader'}
-          ],
-          
-      }
+    entry: {
+        bundle: path.join(srcRoot, './main.js')
+    },
+    output: {
+        path: outputRoot,
+        filename: '[name].js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: [
+                    { loader: 'babel-loader' }
+                ],
+                include: srcRoot
+            },
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: "css-loader"
+                })
+            }
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin("styles.css"),
     ]
-  }
 }
