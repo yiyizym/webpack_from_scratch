7b575cdf222e1a56996ceaf36fde7b83c4be7efd
yiyizym <yiyizym@gmail.com>
2017-10-07 08:40:25 +0800

    add eslint

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,11 @@
 const app = {
-    sayHello: _ => {
+    sayHello: () => {
         let text = document.createElement('span');
         text.textContent = 'Hello!!!';
         document.querySelector('body').appendChild(text);
         console.log(' hello !');
     }
-}
+};
 
 
 module.exports = app;
--- src/js/two.js
+++ src/js/two.js
@@ -1,5 +1,5 @@
 module.exports = {
-    sayHello: _ => {
+    sayHello: () => {
         console.log('I am page two');
     }
-}
+};
